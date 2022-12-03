1e768006f0204cf5d54b9416ebf72825e62abe79
Sebastian Jegerås <jegeras@gmail.com>
2022-12-03 09:48:55 +0100

    Solved day03

--- day03/day03.rs
+++ day03/day03.rs
@@ -1,17 +1,67 @@
+use itertools::Itertools;
 use std::env;
 
 fn solve_part1(inputfile: String) -> usize {
     let contents =
         std::fs::read_to_string(inputfile).expect("Something went wrong reading the file");
 
-    0
+    let to_priority = |item: char| -> u32 {
+        if item.is_lowercase() {
+            1 + (item as u32) - ('a' as u32)
+        } else {
+            27 + (item as u32) - ('A' as u32)
+        }
+    };
+
+    contents
+        .lines()
+        .filter_map(|line| {
+            let line_size = line.len();
+            let (a, b) = line.split_at(line_size / 2);
+
+            for a_ in a.chars() {
+                if b.contains(a_) {
+                    return Some(a_);
+                }
+            }
+            None
+        })
+        .fold(0, |acc, item| acc + to_priority(item) as usize)
 }
 
 fn solve_part2(inputfile: String) -> usize {
     let contents =
         std::fs::read_to_string(inputfile).expect("Something went wrong reading the file");
 
-    0
+    let to_priority = |item: char| -> u32 {
+        if item.is_lowercase() {
+            1 + (item as u32) - ('a' as u32)
+        } else {
+            27 + (item as u32) - ('A' as u32)
+        }
+    };
+
+    contents
+        .lines()
+        .map(|line| line.chars().sorted().unique().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>()
+        .chunks(3)
+        .map(|group| {
+            group
+                .concat()
+                .into_iter()
+                .sorted()
+                .dedup_with_count()
+                .find_map(|(count, badge)| {
+                    if count == 3 {
+                        Some(to_priority(badge))
+                    } else {
+                        None
+                    }
+                })
+                .unwrap() as usize
+        })
+        .sum()
 }
 
 fn main() {
