624b59ed918ffc9ef13b385e4402961c693b5169
Sebastian Jegerås <jegeras@gmail.com>
2022-12-04 12:33:13 +0100

    Solved day04

--- day04/day04.rs
+++ day04/day04.rs
@@ -1,17 +1,56 @@
+use itertools::Itertools;
 use std::env;
 
 fn solve_part1(inputfile: String) -> usize {
     let contents =
         std::fs::read_to_string(inputfile).expect("Something went wrong reading the file");
 
-    0
+    contents
+        .lines()
+        .filter_map(|line| {
+            let mut groups = line.split(",").map(|group| {
+                group
+                    .split("-")
+                    .map(|x| x.parse::<usize>().unwrap())
+                    .collect::<Vec<usize>>()
+            });
+            let a = groups.next().unwrap();
+            let b = groups.next().unwrap();
+
+            if (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1]) {
+                return Some(line);
+            }
+            None
+        })
+        .count()
 }
 
 fn solve_part2(inputfile: String) -> usize {
     let contents =
         std::fs::read_to_string(inputfile).expect("Something went wrong reading the file");
 
-    0
+    contents
+        .lines()
+        .filter_map(|line| {
+            let mut groups = line.split(",").map(|group| {
+                group
+                    .split("-")
+                    .map(|x| x.parse::<usize>().unwrap())
+                    .collect::<Vec<usize>>()
+            });
+            let a = groups.next().unwrap();
+            let b = groups.next().unwrap();
+
+            if (a[0] >= b[0] && a[0] <= b[1])
+                || (a[1] <= b[1] && a[1] >= b[0])
+                || (b[0] >= a[0] && b[0] <= a[1])
+                || (b[0] <= a[1] && b[1] >= a[0])
+            {
+                return Some(line);
+            }
+            None
+        })
+        .count()
 }
 
 fn main() {
