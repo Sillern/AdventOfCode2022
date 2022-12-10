82eed015e99b123890683b10516d20ef77f18f5b
Sebastian Jegerås <jegeras@gmail.com>
2022-12-10 10:13:23 +0100

    Solved day10 part1

--- day10/day10.rs
+++ day10/day10.rs
@@ -1,10 +1,86 @@
 use std::env;
 
+#[derive(Debug, Clone, Copy)]
+enum Instruction {
+    Noop,
+    AddX,
+    Unknown,
+}
+
+type DecodedInstruction = ((Instruction, usize), Option<i64>);
+struct CPU {
+    instructions: Vec<DecodedInstruction>,
+    x: i64,
+    pc: usize,
+    tick: usize,
+    delay: usize,
+}
+
+impl CPU {
+    pub fn new(program: String, start_value: i64) -> Self {
+        let instructions = program
+            .lines()
+            .map(|line| {
+                let mut tokens = line.split(" ");
+                let instruction_opt = tokens.next().unwrap();
+                let value_opt = match tokens.next() {
+                    Some(value) => Some(value.parse::<i64>().unwrap()),
+                    None => None,
+                };
+
+                match instruction_opt {
+                    "noop" => ((Instruction::Noop, 1), None),
+                    "addx" => ((Instruction::AddX, 2), value_opt),
+                    _ => ((Instruction::Unknown, 0), None),
+                }
+            })
+            .collect::<Vec<DecodedInstruction>>();
+
+        Self {
+            instructions,
+            x: start_value,
+            pc: 0,
+            tick: 0,
+            delay: 0,
+        }
+    }
+
+    pub fn value(&self) -> i64 {
+        self.x
+    }
+
+    pub fn tick(&mut self) {
+        let ((instruction, cycle_count), value_opt) = self.instructions[self.pc];
+        if self.delay == cycle_count {
+            match instruction {
+                Instruction::Noop => {}
+                Instruction::AddX => self.x += value_opt.unwrap(),
+                Instruction::Unknown => panic!(),
+            }
+            self.delay = 1;
+            self.pc += 1;
+        } else {
+            self.delay += 1;
+        }
+    }
+}
+
 fn solve_part1(inputfile: String) -> usize {
     let contents =
         std::fs::read_to_string(inputfile).expect("Something went wrong reading the file");
 
-    0
+    let mut cpu = CPU::new(contents, 1);
+
+    let mut result = 0;
+    let thresholds = vec![20, 60, 100, 140, 180, 220];
+    for tick in 0..221 {
+        if thresholds.contains(&tick) {
+            result += tick * cpu.value();
+        }
+
+        cpu.tick();
+    }
+    result as usize
 }
 
 fn solve_part2(inputfile: String) -> usize {
