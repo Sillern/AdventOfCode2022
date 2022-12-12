ddf5ff839521276d1dc121b8aef2253117f1106f
Sebastian Jegerås <jegeras@gmail.com>
2022-12-12 22:17:08 +0100

    Add simple visualization of day12

--- day12/day12.rs
+++ day12/day12.rs
@@ -1,8 +1,84 @@
+use image::ImageBuffer;
 use std::collections::HashMap;
 use std::env;
 
 type Coordinate = (i32, i32);
 type HeightMap = HashMap<Coordinate, usize>;
+
+type Color = (u8, u8, u8);
+
+fn draw_pixel(pixels: &mut Vec<(Coordinate, Color)>, position: Coordinate, color_index: i32) {
+    let color = match color_index {
+        100 => (219, 109, 80),
+        _ => (
+            (120 + color_index * 5) as u8,
+            (100 + color_index * 5) as u8,
+            (50 + color_index * 5) as u8,
+        ),
+    };
+
+    pixels.push((position, color));
+}
+
+fn draw_map(map: &HeightMap, path: &Vec<Coordinate>, frame: u32) {
+    let path_x_min = path.iter().map(|pos| pos.0).min().unwrap();
+    let path_x_max = path.iter().map(|pos| pos.0).max().unwrap();
+    let path_y_min = path.iter().map(|pos| pos.1).min().unwrap();
+    let path_y_max = path.iter().map(|pos| pos.1).max().unwrap();
+    let x_min = path_x_min.min(map.iter().map(|(pos, _)| pos.0).min().unwrap_or(0));
+    let x_max = path_x_max.max(map.iter().map(|(pos, _)| pos.0).max().unwrap_or(0));
+    let y_min = path_y_min.min(map.iter().map(|(pos, _)| pos.1).min().unwrap_or(0));
+    let y_max = path_y_max.max(map.iter().map(|(pos, _)| pos.1).max().unwrap_or(0));
+    let x_range = (x_max - x_min) as u32;
+    let y_range = (y_max - y_min) as u32;
+    let dimensions: Coordinate = (32.max(1 + x_range as i32), 32.max(1 + y_range as i32));
+
+    let scale = 8;
+    let border = 2;
+    let real_size = (
+        scale * (border * 2 + dimensions.0 as u32),
+        scale * (border * 2 + dimensions.1 as u32),
+    );
+
+    let mut pixels = Vec::<(Coordinate, Color)>::new();
+
+    for (pos, height) in map.iter() {
+        draw_pixel(&mut pixels, *pos, *height as i32);
+    }
+    for pos in path.iter() {
+        draw_pixel(&mut pixels, *pos, 100);
+    }
+
+    let mut img = ImageBuffer::from_fn(real_size.0, real_size.1, |_x, _y| {
+        image::Rgb([255, 255, 255])
+    });
+
+    for ((x, y), color) in pixels {
+        let pixel = image::Rgb([color.0, color.1, color.2]);
+        let draw_pos = (
+            scale as i32 * (border as i32 + (x - x_min)),
+            scale as i32 * (border as i32 + (y - y_min)),
+        );
+
+        if draw_pos.0 >= 0
+            && draw_pos.1 >= 0
+            && draw_pos.0 < real_size.0 as i32
+            && draw_pos.1 < real_size.1 as i32
+        {
+            for offset_y in 0..scale {
+                for offset_x in 0..scale {
+                    img.put_pixel(
+                        draw_pos.0 as u32 + offset_x,
+                        draw_pos.1 as u32 + offset_y,
+                        pixel,
+                    );
+                }
+            }
+        }
+    }
+
+    img.save(format!("frames/day12.frame{:05}.png", frame));
+}
 
 fn get_shortest_path(map: &HeightMap, start: Coordinate, end: Coordinate) -> Option<usize> {
     let mut queue: Vec<((Coordinate, usize), Vec<Coordinate>)> =
@@ -10,6 +86,7 @@ fn get_shortest_path(map: &HeightMap, start: Coordinate, end: Coordinate) -> Opt
 
     let mut visited: Vec<Coordinate> = Vec::new();
 
+    let mut frame = 0;
     while !queue.is_empty() {
         // sort by length
         queue.sort_by_key(|((_, height), path)| (26 - (*height as usize)) * path.len());
@@ -23,7 +100,11 @@ fn get_shortest_path(map: &HeightMap, start: Coordinate, end: Coordinate) -> Opt
                     visited.push(position.clone());
                 }
 
+                draw_map(map, &came_from, frame);
+                frame += 1;
+
                 if position == end {
+                    draw_map(map, &came_from, frame);
                     return Some(came_from.len() - 1);
                 } else {
                     [(0, -1), (0, 1), (-1, 0), (1, 0)].iter().for_each(|step| {
